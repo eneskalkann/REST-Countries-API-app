31760fd6debba15a2cbf8999a0ac5e852ed1479d
Muhammet Enez Kalkan <75678744+eneskalkann@users.noreply.github.com>
2023-04-16 17:47:45 +0300

    add

---
+++ src/components/Filter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Filter() {
+  return (
+    <div className="flex justify-between items-center space-y-20">
+      <h1>search bar</h1>
+      <h1>region</h1>
+    </div>
+  );
+}
+
+export default Filter;
