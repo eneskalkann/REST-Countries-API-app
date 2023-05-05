56dc4ebcce5ff8bd540b60087271f5c094864535
Muhammet Enez Kalkan <75678744+eneskalkann@users.noreply.github.com>
2023-05-05 21:52:47 +0300

    fixed: dark mode disabled fn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ function Header() {
   return (
     <div className="flex justify-between py-8 bg-white shadow-sm">
       <div className="text-lg font-extrabold md:text-2xl">
-       <Link to="/">Where in the world?</Link>
+        <Link to="/">Where in the world?</Link>
       </div>
-      <div className="flex items-center gap-2 text-sm cursor-pointer md:text-base">
+      {/* <div className="flex items-center gap-2 text-sm cursor-pointer md:text-base">
         <MdOutlineDarkMode />
         <span>Dark Mode</span>
-      </div>
+      </div> */}
     </div>
   );
 }
