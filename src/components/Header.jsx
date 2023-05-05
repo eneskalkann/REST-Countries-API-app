fbd4c07fd857de24d5eff10721ebb56b286e8139
Muhammet Enez Kalkan <75678744+eneskalkann@users.noreply.github.com>
2023-05-05 22:00:01 +0300

    setToggle add for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineDarkMode, MdDarkMode } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [toggle, setToggle] = useState("light");
+  const theme = () => {
+    if (theme === "light") {
+      setToggle("dark");
+    } else {
+      setToggle("dark");
+    }
+  };
   return (
     <div className="flex justify-between py-8 bg-white shadow-sm">
       <div className="text-lg font-extrabold md:text-2xl">
         <Link to="/">Where in the world?</Link>
       </div>
-      {/* <div className="flex items-center gap-2 text-sm cursor-pointer md:text-base">
+      <div className="flex items-center gap-2 text-sm cursor-pointer md:text-base">
         <MdOutlineDarkMode />
         <span>Dark Mode</span>
-      </div> */}
+      </div>
     </div>
   );
 }
